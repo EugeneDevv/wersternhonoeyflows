10f22576e2e48004e088d0aa3b7852444968080f
EugeneDevv <eugenemrlens@gmail.com>
2020-06-03 01:39:50 +0300

    Products Section Complete

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
+    $toggleCollapse = $('.fa-bars');
 
     // click event on toggle menu
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
 
-
+    // Owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
 });
 
 // const selectElement = (s) => document.querySelector(s);
